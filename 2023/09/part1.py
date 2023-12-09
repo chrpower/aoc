05122cce79ee8268cb1547ec12a6994e75c2cd48
Christopher Power <chrpower.dev@gmail.com>
2023-12-09 22:56:17 +0100

    Day 9 Solve: Implement OASIS Analysis for Forward and Backward Environmental Extrapolation

---
+++ 2023/09/part1.py
@@ -0,0 +1,8 @@
+def predict(seq):
+    if sum(seq) == 0:
+        return 0
+    return seq[-1] + predict([y - x for x, y in zip(seq, seq[1:])])
+
+
+with open("input/input.txt", "r") as file:
+    print(sum(predict(list(map(int, line.split()))) for line in file))
